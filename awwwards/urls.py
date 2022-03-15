e1d0ad74e12ab2b6cd535c4a61c8f50b55020ec0
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-15 11:48:23 +0300

    Added the profile url path

--- awwwards/urls.py
+++ awwwards/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
     path('', include('projects.urls'))
 ]
 
