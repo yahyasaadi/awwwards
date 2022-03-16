4873fbba83281dd4c8c0cafc186798012c1da817
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 10:27:22 +0300

    Added a login url path

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -130,6 +130,8 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
