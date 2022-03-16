b39be3cb27abbc10b03035159a15c9229280e7fa
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 10:51:08 +0300

    Added the url to list projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProjectCreateView
+from .views import ProjectListView, ProjectCreateView
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
+    path('home/', ProjectListView.as_view(), name='home'),
     path('project/new/', ProjectCreateView.as_view(), name='new-project'),
 ]
