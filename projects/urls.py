1d227af263cce85a1614d62a2e6775d98d03be18
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 11:42:51 +0300

    Added an api url

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from .views import ProjectListView, ProjectCreateView
-from . import views
+from django.urls import path, include
+from .views import (ProjectListView,
+                    ProjectCreateView,
+                    ProfileViewSet,
+                    ProjectViewSet
+                )
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'profiles', ProfileViewSet, basename='profiles')
+router.register(r'projects', ProjectViewSet, basename='projects')
 
 urlpatterns = [
     # path('home/', views.home, name='home'),
     path('home/', ProjectListView.as_view(), name='home'),
     path('project/new/', ProjectCreateView.as_view(), name='new-project'),
+    path('api/', include(router.urls)),
 ]
