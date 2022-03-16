f10a5e2499dc760c08cac0dc400fa32b5d9824fe
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 11:43:12 +0300

    Added api views

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView
+from rest_framework import viewsets, permissions
+from users.models import Profile
 from .models import Project
+from .serializers import ProfileSerializer, ProjectSerializer
 
 # Create your views here.
 def home(request):
@@ -20,3 +23,15 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+# API Views
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
