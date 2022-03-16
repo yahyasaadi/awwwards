08ce86451cecd6070be12025b794fc48db16304b
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 12:31:06 +0300

    Added new urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .views import (ProjectListView,
                     ProjectCreateView,
+                    ProjectDetailView,
+                    UserProjectListView,
                     ProfileViewSet,
                     ProjectViewSet
                 )
+from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -14,5 +17,8 @@ urlpatterns = [
     # path('home/', views.home, name='home'),
     path('home/', ProjectListView.as_view(), name='home'),
     path('project/new/', ProjectCreateView.as_view(), name='new-project'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('user/<str:username>', UserProjectListView.as_view(), name='user-projects'),
+    path('search/', views.search, name='search'),
     path('api/', include(router.urls)),
 ]
