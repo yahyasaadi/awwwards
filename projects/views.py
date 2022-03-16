07173b4d0e4836cdabb73f185d7c1d152bb8d9ac
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 10:29:56 +0300

    Added new projects view

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
+from .models import Project
 
 # Create your views here.
 def home(request):
     return render(request, 'projects/base.html')
+
+
+# Post a project
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project 
+    fields = ['title', 'image', 'description']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
