1d1a0117e8c81876020b307f347d3c0f68765e8a
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 10:31:23 +0300

    Added the url for a new project

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import ProjectCreateView
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('project/new/', ProjectCreateView.as_view(), name='new-project'),
 ]
