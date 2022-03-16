8979b962bc9d639ecad6c8cbeed83dc8b56fdc4d
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 12:31:37 +0300

    Added a search view

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView
+from django.contrib.auth.models import User
+from django.views.generic import ListView, CreateView, DetailView
 from rest_framework import viewsets, permissions
 from users.models import Profile
 from .models import Project
@@ -23,6 +24,38 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+
+
+
+class UserProjectListView(ListView):
+    model = Project # blog/post_list.html requires this template to run
+    template_name = 'blog/user_projects.html' # Or specify the template to use here
+    context_object_name = 'project'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Project.objects.filter(author=user).order_by('-date_posted')
+
+
+
+
+# Search method
+def search(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		projects = Project.objects.filter(author__username__contains=searched)
+	
+		return render(request, 'projects/search.html', {'projects':projects})
+	else:
+		return render(request, 'projects/search.html')
+
+
+
 
 # API Views
 class ProfileViewSet(viewsets.ModelViewSet):
