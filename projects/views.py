3af9e5a7953a5aae205eeb90142c4c37c206e587
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-16 10:52:22 +0300

    Added a view to list projects

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
+from django.views.generic import ListView, CreateView
 from .models import Project
 
 # Create your views here.
 def home(request):
     return render(request, 'projects/base.html')
 
+
+class ProjectListView(ListView):
+    model = Project
+    ordering = ['-date_posted']
 
 # Post a project
 class ProjectCreateView(LoginRequiredMixin, CreateView):
